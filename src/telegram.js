78b04ebae9bdc0257c88f0bab36d8d0503ae944b
Tim Ermilov <yamalight@gmail.com>
2019-04-06 14:56:14 +0200

    Move database cleaning into separate telegram command

--- src/telegram.js
+++ src/telegram.js
@@ -12,16 +12,22 @@ bot.on('channel_post', ctx => {
 });
 
 bot.command('dump', async ctx => {
-  const {from} = ctx.update.message;
   const posts = await getPosts();
   if (!posts.length) {
     ctx.reply('No saved messages yet!');
     return;
   }
   ctx.reply(posts.join('\n'));
-  if (from.username === owner) {
-    await clearDb();
+});
+
+bot.command('clear', async ctx => {
+  const {from} = ctx.update.message;
+  if (from.username !== owner) {
+    ctx.reply('Sorry! Only Tim is allowed to do that :)');
+    return;
   }
+  await clearDb();
+  ctx.reply('Done. Database cleared.');
 });
 
 module.exports = () => {
